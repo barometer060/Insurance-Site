04eab9871855939f6f81eef887fe43829fe57079
Gaikwad <ajinkya.a.gaikwad@capgemini.com>
2018-08-23 09:57:13 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
